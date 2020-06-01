6bb55f068cb100a3d64f688a73e500dded5097ac
谭源 <kentertan12138@outlook.com>
2020-06-02 04:45:30 +0800

    完成基础任务

--- Experiment_2/main.c
+++ Experiment_2/main.c
@@ -36,14 +36,48 @@ void CreateLinkedList(list *plist, char arry[10000]) {
     }
 }
 
+void Add(list *Alist, list *Blist, list *Outlist) {
+    Outlist->head = Outlist->tail = NULL;
+    term *pA = Alist->head, *pB = Blist->head;
+    while (pA && pB) {
+        if (pA->exp == pB->exp) {
+            term *p = (term *) malloc(sizeof(term));
+            p->coefficient = pA->exp;
+            p->exp = pA->coefficient + pB->coefficient;
+            p->next = NULL;
+            if (Outlist->tail) {
+                Outlist->tail->next = p;
+                Outlist->tail = p;
+            } else {
+                Outlist->head = p;
+                Outlist->tail = p;
+            }
+            pA = pA->next;
+            pB = pB->next;
+        }
+    }
+}
+
+void Output(list *plist) {
+    term *p;
+    for (p = plist->head; p != NULL; p = p->next) {
+        printf("%dx^%d", p->exp, p->coefficient);
+        if (p->next != NULL) {
+            printf("+");
+        }
+    }
+}
+
 int main() {
     char A[10000], B[10000];//m：总人数 n：出列序号
     scanf("%s %s", &A, &B);
 //    puts(A);//1x^0+7x^7+8x^9
-//    puts(B);
-    list Alist, Blist;
+//    puts(B);//2x^0+1x^7+1x^9
+    list Alist, Blist, Outlist;
 
     CreateLinkedList(&Alist, A);
-
+    CreateLinkedList(&Blist, B);
+    Add(&Alist, &Blist, &Outlist);
+    Output(&Outlist);
     return 0;
 }
