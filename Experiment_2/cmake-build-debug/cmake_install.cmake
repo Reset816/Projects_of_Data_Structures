8e8ca9ba6bc9b44530e91fc51362e74bf500d26e
谭源 <kentertan12138@outlook.com>
2020-06-02 04:19:46 +0800

    提交其他文件

--- Experiment_2/cmake-build-debug/cmake_install.cmake
+++ Experiment_2/cmake-build-debug/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Experiment_1")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Experiment_2")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
