4f1296647d5bca0bcdd3a36f58ba26174b2aa6b4
谭源 <40438607+reset12138@users.noreply.github.com>
2020-06-09 12:04:27 +0800

    初步实现栈

--- Experiment_3/main.c
+++ Experiment_3/main.c
@@ -1,6 +1,52 @@
 #include <stdio.h>
+#include <malloc.h>
+
+typedef struct lineStack {
+    int data;
+    struct lineStack *next;
+    struct lineStack *previous;
+} lineStack;
+
+typedef struct Stack {
+    lineStack *top;
+    lineStack *buttom;
+} Stack;
+
+void push(Stack *plist, int a) {
+    lineStack *p = (lineStack *) malloc(sizeof(lineStack));
+    p->data = a;
+    p->next = NULL;
+    if (plist->buttom) {
+        plist->buttom->next = p;
+        plist->buttom = p;
+    } else {
+        plist->top = p;
+        plist->buttom = p;
+    }
+}
+
+void pop(Stack *plist) {
+    if (plist) {
+        lineStack *p = plist->top;
+        printf("pop element:%d ", p->data);
+        p->previous->next = NULL;
+        plist->top = p->previous;
+        free(p);
+    } else {
+        printf("no element in stack");
+    }
+}
 
 int main() {
-    printf("Hello, World!\n");
+    printf("Type in the Arithmetic Expression\nFor Example:\n1+3*4-6/2\n");
+    Stack TEST;
+    TEST.buttom = NULL;
+    push(&TEST, 1);
+    push(&TEST, 2);
+    push(&TEST, 3);
+//    pop(&TEST);
+//    pop(&TEST);
+//    pop(&TEST);
+//    pop(&TEST);
     return 0;
 }
