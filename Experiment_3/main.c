b9f290cabf0670b77ed09aea6d55ac954b359074
谭源 <40438607+reset12138@users.noreply.github.com>
2020-06-10 14:40:45 +0800

    完成框架

--- Experiment_3/main.c
+++ Experiment_3/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <malloc.h>
+#include <stdbool.h>
 
 typedef struct lineStack {
     int data;
@@ -12,9 +13,9 @@ typedef struct Stack {
     lineStack *buttom;
 } Stack;
 
-void push(Stack *plist, int a) {
+void push(Stack *plist, char value) {
     lineStack *p = (lineStack *) malloc(sizeof(lineStack));
-    p->data = a;
+    p->data = value;
     p->up = NULL;
     p->down = plist->top;
     if (plist->top) {
@@ -42,16 +43,71 @@ void pop(Stack *plist) {
     }
 }
 
+bool isInOP(char tmp) {
+    if (tmp == '+' || tmp == '-' || tmp == '*' || tmp == '/') {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+char ComparePriority(char a, char b) {
+    if (b == '(') {
+        return '<';
+    } else if (b == ')') {
+        if (a == '(') {
+            return '=';
+        } else {
+            return '>';
+        }
+    } else if (b == '+' || b == '-') {
+        if (a == '(') {
+            return '<';
+        } else {
+            return '>';
+        }
+    } else if (b == '*' || b == '/') {
+        if (a == '+' || a == '-' || a == '(') {
+            return '<';
+        } else {
+            return '>';
+        }
+    }
+}
+
+void scan(Stack *pOPTR, Stack *pOPTD) {
+    char tmp;
+    while (1) {
+        //8x^9+7x^7+1x^0 count=1是系数，count=4是指数,count=6是系数，count=9是指数
+        tmp = getchar();
+        if (tmp != '\n') {
+            if (isInOP(tmp)) {//如果读入的是运算符
+                switch (ComparePriority(pOPTR->top, tmp)) {
+                    case '<' :
+                        push(pOPTR, tmp);
+                        break;
+                    case '=' :
+                        break;
+                    case '>' :
+                        break;
+                }
+
+            } else {//如果读入的不是运算符
+                push(pOPTD, tmp);
+            }
+
+        } else {
+            return;
+        }
+    }
+}
+
 int main() {
     printf("Type in the Arithmetic Expression\nFor Example:\n1+3*4-6/2\n");
-    Stack TEST;
-    TEST.top = NULL;
-    push(&TEST, 1);
-    push(&TEST, 2);
-    push(&TEST, 3);
-    pop(&TEST);
-    pop(&TEST);
-    pop(&TEST);
-    pop(&TEST);
+    Stack OPTR, OPTD;
+    OPTR.top = NULL;
+    OPTD.top = NULL;
+    scan(&OPTR, &OPTD);
     return 0;
 }
