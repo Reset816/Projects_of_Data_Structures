7608b7a47b7a1567261d35d39a58e7c174556bf7
谭源 <40438607+reset12138@users.noreply.github.com>
2020-06-29 10:11:08 +0800

    完成非递归先序

--- Experiment_6/non_recursive.h
+++ Experiment_6/non_recursive.h
@@ -8,9 +8,9 @@
 void non_Recursive_InOrder(BinaryTree &T) {
     Stack storage_node;
 
-    BinaryTree Search_Pointer = T;
+    BinaryTree Search_Pointer = T;//搜索指针
     do {
-        while (Search_Pointer != NULL) {//不把#入栈
+        while (Search_Pointer != NULL) {//遍历左子树 不把#入栈
             push(&storage_node, Search_Pointer);
             Search_Pointer = Search_Pointer->lchild;
         }
@@ -18,8 +18,8 @@ void non_Recursive_InOrder(BinaryTree &T) {
             //此时Search_Pointer指向NULL
             Search_Pointer = storage_node.top->ElementofStack;
             //此时Search_Pointer指向最底层左结点
-            pop(&storage_node);
-            Visit(Search_Pointer);
+            pop(&storage_node);//退栈
+            Visit(Search_Pointer);//访问根
             Search_Pointer = Search_Pointer->rchild;
         }
     } while (!isEmpty(storage_node) || Search_Pointer != NULL);
@@ -29,9 +29,10 @@ void non_Recursive_PreOrder(BinaryTree &T) {
     Stack storage_node;
 
     BinaryTree Search_Pointer = T;
-    do {
+    do {//把访问的结点压栈，然后访问他的左节点。取出栈顶，访问它的右节点
         while (Search_Pointer != NULL) {//不把#入栈
             push(&storage_node, Search_Pointer);
+            Visit(Search_Pointer);
             Search_Pointer = Search_Pointer->lchild;
         }
         if (!isEmpty(storage_node)) {
@@ -39,7 +40,6 @@ void non_Recursive_PreOrder(BinaryTree &T) {
             Search_Pointer = storage_node.top->ElementofStack;
             //此时Search_Pointer指向最底层左结点
             pop(&storage_node);
-            Visit(Search_Pointer);
             Search_Pointer = Search_Pointer->rchild;
         }
     } while (!isEmpty(storage_node) || Search_Pointer != NULL);
