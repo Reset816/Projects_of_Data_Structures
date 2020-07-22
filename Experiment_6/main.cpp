e9f492d23b6d417af9efb7bbf21def083d7ef732
谭源 <40438607+reset12138@users.noreply.github.com>
2020-07-23 02:58:00 +0800

    working on the last part

--- Experiment_6/main.cpp
+++ Experiment_6/main.cpp
@@ -23,6 +23,7 @@ void init(int m, head row[]) {
     }
 }
 
+stack<int> REVERSE_TOPOLOGICAL_SORTING_RESULT;
 stack<int> TOPOLOGICAL_SORTING_RESULT;
 stack<int> CRITICAL_ACTIVITY;
 
@@ -40,7 +41,7 @@ void get_ve(int n, head row[], int ve[]) {
     while (!S.empty()) {
         now = S.front();
         S.pop();
-        TOPOLOGICAL_SORTING_RESULT.push(now);
+        REVERSE_TOPOLOGICAL_SORTING_RESULT.push(now);//这样构造的是逆向的拓扑排序
         node *P = row[now].firstnode;
         while (P) {
             row[P->adjvex].indegree--;
@@ -59,15 +60,16 @@ void get_ve(int n, head row[], int ve[]) {
 
 void get_vl_and_e_and_l(int n, head *row, int *ve, int *vl, int *e, int *l) {
     int now;
-    vl[TOPOLOGICAL_SORTING_RESULT.top()] = ve[TOPOLOGICAL_SORTING_RESULT.top()];
-    while (!TOPOLOGICAL_SORTING_RESULT.empty()) {
-        now = TOPOLOGICAL_SORTING_RESULT.top();
-        TOPOLOGICAL_SORTING_RESULT.pop();
+    vl[REVERSE_TOPOLOGICAL_SORTING_RESULT.top()] = ve[REVERSE_TOPOLOGICAL_SORTING_RESULT.top()];
+    while (!REVERSE_TOPOLOGICAL_SORTING_RESULT.empty()) {
+        now = REVERSE_TOPOLOGICAL_SORTING_RESULT.top();
+        REVERSE_TOPOLOGICAL_SORTING_RESULT.pop();
+        TOPOLOGICAL_SORTING_RESULT.push(now);
         node *P = row[now].firstnode;
 
         while (P) {
-            //vl[now] = min(vl[P->adjvex] - P->weight, vl[now]);
-            vl[now] = vl[P->adjvex] - P->weight;
+            vl[now] = min(vl[P->adjvex] - P->weight, vl[now]);
+            //vl[now] = vl[P->adjvex] - P->weight;
             e[P->id_of_edge] = ve[now];//get e
             l[P->id_of_edge] = vl[P->adjvex] - P->weight;//get l
             if (e[P->id_of_edge] == l[P->id_of_edge])
@@ -81,13 +83,30 @@ void get_vl_and_e_and_l(int n, head *row, int *ve, int *vl, int *e, int *l) {
 //    }
 }
 
-void PRINT_CRITICAL_ACTIVITY(){
+void PRINT_CRITICAL_ACTIVITY() {
+    cout << "Critical Activity:";
     while (!CRITICAL_ACTIVITY.empty()) {
         cout << CRITICAL_ACTIVITY.top() << " ";
         CRITICAL_ACTIVITY.pop();
     }
 }
 
+void PRINT_CRITICAL_PATH(head *row, int *e, int *l) {
+    cout << "Critical Path:";
+    int now;
+    while (!CRITICAL_ACTIVITY.empty()) {
+        now = CRITICAL_ACTIVITY.top();
+        CRITICAL_ACTIVITY.pop();
+        node *P = row[now].firstnode;
+        while (P) {
+            if (e[P->id_of_edge] == l[P->id_of_edge]) {
+                cout << P->adjvex << " ";
+                continue;
+            }
+            P = P->nextnode;
+        }
+    }
+}
 
 int main() {
     int n, m;
@@ -100,7 +119,7 @@ int main() {
     memset(vl, 0x3f, sizeof(vl));
 
     get_ve(n, row, ve);
-    cout << endl;
+    //cout << endl;
     get_vl_and_e_and_l(n, row, ve, vl, e, l);
     //test e
 //    for (int i = 1; i <= m; i++) {
@@ -110,6 +129,9 @@ int main() {
 //    for (int i = 1; i <= m; i++) {
 //        cout << i << " " << l[i] << endl;
 //    }
+    //PRINT_CRITICAL_ACTIVITY();
+    cout << endl;
+    PRINT_CRITICAL_PATH(row, e, l);
     return 0;
 }
 
